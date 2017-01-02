a6376d15daacccddc3912e20d786e7caa782a6cc
Dominik Süß <dominik.suess@outlook.at>
2017-01-02 21:19:58 +0100

    Added notifications

--- web/main.js
+++ web/main.js
@@ -9,9 +9,58 @@ Vue.component('worker-item', {
     props: ['worker'],
     template:'#worker-item'
 });
+const NotificationStore = {
+    state: [], // here the notifications will be added
 
-var workerlist = new Vue({
+    addNotification: function (notification) {
+        this.state.push(notification);
+    },
+    removeNotification: function (notification) {
+        var index = this.state.indexOf(notification);
+        this.state.splice(index,1);
+    }
+};
+
+var Notification = Vue.extend({
+	  template: '#notification',
+    props: ['notification'],
+    data: function () {
+  	    return { timer: null };
+	  },
+    mounted: function () {
+        let timeout = this.notification.hasOwnProperty('timeout') ? this.notification.timeout : true;
+        if (timeout) {
+  	        let delay = this.notification.delay || 3000;
+            this.timer = window.setTimeout(function () {
+                this.triggerClose(this.notification);
+            }.bind(this), delay);
+        }
+    },
+
+    methods: {
+        triggerClose: function (notification) {
+    	      clearTimeout(this.timer);
+            NotificationStore.removeNotification(notification);
+        }
+    }
+});
+var Notifications = Vue.extend({
+	  template: '#notifications',
+    components: {
+        notification: Notification
+    },
+    data () {
+        return {
+            notifications: NotificationStore.state
+        };
+    }
+});
+
+var app = new Vue({
     el: '#app',
+    components: {
+  	    'notifications': Notifications
+    },
     data: {
         workers: [],
         accept: '',
@@ -53,11 +102,20 @@ var workerlist = new Vue({
                     });
                     this.workers = workers;
                 });
+        },
+        notifyDone(filename){
+            NotificationStore.addNotification({
+      	        title: 'Download Finished',
+                text: `${filename} has finished downloading`,
+                type: "is-primary",
+                timeout: true,
+                delay: 10000
+            });
         }
     }
 });
 
-workerlist.refreshWorkers();
+app.refreshWorkers();
 
 var loc = window.location;
 var uri = '';
@@ -72,10 +130,11 @@ var ws = new WebSocket(uri);
 
 ws.onmessage = (evt) => {
     var status = JSON.parse(evt.data);
-    var workeritem = workerlist.$refs[`worker-${status.workerid}`];
+    var workeritem = app.$refs[`worker-${status.workerid}`];
     if(workeritem != undefined) {
         status.isFinished = () => {
             if(status.total === status.length) {
+                app.notifyDone(workeritem[0].worker.filename);
                 return true;
             } else {
                 return false;
